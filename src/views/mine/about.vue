7aa5e5524883c579f6d1fc962f11f15dc4d284db
ddd615 <670232662@qq.com>
2019-12-04 19:10:29 +0800

    12.4 19:10

--- src/views/mine/about.vue
+++ src/views/mine/about.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="about">
-      <img src="../../assets/images/about_nav.png" alt="" width="100%" style="display: block">
+      <img :src="info.backgroundMap" alt="" width="100%" style="display: block">
       <van-cell-group>
         <van-cell :title="'电话：'+info.mobile"/>
         <van-cell :title="'邮箱：'+info.email"/>
         <van-cell :title="'地址：'+info.address"/>
       </van-cell-group>
       <div style="height: 10px;background: #F2F2F2"></div>
-      <img :src="info.backgroundMap" alt="地图" width="92%" style="margin: 10px auto">
+      <div id="container"></div>
     </div>
 </template>
 
@@ -20,12 +20,28 @@
           }
       },
       created() {
-          this.getInfo();
+
+
+      },
+      mounted(){
+        this.getInfo();
       },
       methods:{
           getInfo(){
             this.$get('/system/info/1',{},res => {
               this.info = res.data.data;
+              console.log(this.info);
+              console.log(parseFloat(this.info.lat));
+              console.log(parseFloat(this.info.lon));
+              var map = new AMap.Map('container', {
+                zoom:11,//级别
+                center: [parseFloat(this.info.lon), parseFloat(this.info.lat)],//中心点坐标
+                viewMode:'3D'//使用3D视图
+              });
+              var marker = new AMap.Marker({
+                position:[parseFloat(this.info.lon), parseFloat(this.info.lat)]//位置
+              })
+              map.add(marker);
             })
           }
       }
@@ -40,5 +56,10 @@
   .about{
     height: 100vh;
     background: #ffffff;
+  }
+  #container {
+    width:92%;
+    height: 200px;
+    margin: 20px auto;
   }
 </style>
